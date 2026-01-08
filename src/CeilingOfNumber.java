fd0b74344d0e3b18bdaea7d6739bfce5f1ca303f
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-01-08 22:52:56 +0530

    Understanding and Evaluating Ceiling of a Number in a Ascending Sorted Array

---
+++ src/CeilingOfNumber.java
@@ -0,0 +1,30 @@
+public class CeilingOfNumber {
+     /*
+     In Sorted Ascending Array Ceiling of Number is The smallest 
+    element in the array that is greater than or equal to target element
+     */
+    public static void main(String[] args) {
+        int[] A1={2,3,5,9,14,16,18};
+        System.out.println(ceiling(A1,4));
+    }
+    public static int ceiling(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+       
+        if(target>arr[end]){
+            return -1;
+        }
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return target;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }else if(arr[mid]<target){
+                start=mid+1;
+            }
+        }
+        return arr[start];
+    }
+}
