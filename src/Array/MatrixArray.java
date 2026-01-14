e3853a8936217d9464a035ba676e74c09f21de2b
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-01-14 22:57:14 +0530

    Comment Added

--- src/Array/MatrixArray.java
+++ src/Array/MatrixArray.java
@@ -5,7 +5,7 @@ public class MatrixArray{
         int[][] a={
             {2,3,5,7},
             {34,35,56,78},
-            {80,81,82,83}  //sorted 2D Array row wise and column wise
+            {80,81,82,83}  //sorted 2D Array row wise and column wise //Time Complexity=O(N)
         };
         System.out.println(Arrays.toString(search(a, 83)));
     }
