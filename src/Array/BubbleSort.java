044b4f13ac81776e6af981871c1bf56fba87c926
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-01-15 22:55:27 +0530

    Concept of Bubble Sort

--- src/Array/BubbleSort.java
+++ src/Array/BubbleSort.java
@@ -1,26 +1,27 @@
-import java.util.Arrays;
+    import java.util.Arrays;
 
-//Time Complexity = O(N^2)
-/*Bubble Sort is a simple comparison-based sorting algorithm. 
-It repeatedly steps through the list, compares adjacent elements, 
-and swaps them if they are in the wrong order. */
+    //Time Complexity = O(N^2)
+    /*Bubble Sort is a simple comparison-based sorting algorithm. 
+    It repeatedly steps through the list, compares adjacent elements, 
+    and swaps them if they are in the wrong order. */
 
-public class BubbleSort {
-    public static void main(String[] args) {
-        int[] a={52,4,5,67,42};
-        System.out.println(Arrays.toString(sort(a)));
-    }
-    public static int[] sort(int[] arr){
-      
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length;j++){
-                if(arr[j]<arr[j-1]){
-                    int temp=arr[j-1];
-                    arr[j-1]=arr[j];
-                    arr[j]=temp;
+    public class BubbleSort {
+        public static void main(String[] args) {
+            int[] a={52,4,5,67,42};
+            System.out.println(Arrays.toString(sort(a)));
+        }
+        public static int[] sort(int[] arr){
+        
+            for(int i=0;i<arr.length;i++){
+                for(int j=1;j<arr.length;j++){
+                    if(arr[j]<arr[j-1]){
+                        int temp=arr[j-1];
+                        arr[j-1]=arr[j];
+                        arr[j]=temp;
+                    }
+                    
                 }
             }
+            return arr;
         }
-        return arr;
     }
-}
