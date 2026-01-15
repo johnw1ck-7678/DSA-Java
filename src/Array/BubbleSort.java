db06c4e7fb47f11cda09fef7bd1db593b07ffd0f
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-01-15 22:20:32 +0530

    Concpet of Bubble Sort

---
+++ src/Array/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+//Time Complexity = O(N^2)
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a={52,4,5,67,42};
+        System.out.println(Arrays.toString(sort(a)));
+    }
+    public static int[] sort(int[] arr){
+      
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
