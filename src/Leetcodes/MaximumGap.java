623de56ba5f3745930460e12d47f1b4473a7522f
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-01-28 02:51:36 +0530

    Maximum Gap

---
+++ src/Leetcodes/MaximumGap.java
@@ -0,0 +1,25 @@
+package Leetcodes;
+//https://leetcode.com/problems/maximum-gap/?envType=problem-list-v2&envId=radix-sort
+
+public class MaximumGap {
+    public static void main(String[] args) {
+        int[] A={3,6,9,1};
+        int[] B={10};
+        System.out.println(maximumGap(A));
+        System.out.println(maximumGap(B));
+        
+    }
+    
+    public static int maximumGap(int[] nums){
+        if(nums.length==1){  //Given Constraints says array length goes from 1.
+            return 0;
+        }
+        int count=0;
+        int a=0;
+        while(a<nums.length-1){
+            count=Math.max(count,nums[a+1]-nums[a]);
+            a++;
+        }
+        return count;
+    }
+}
