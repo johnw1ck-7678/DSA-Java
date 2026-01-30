32219c5462688a6937d62c562fcb074b6191643e
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-01-30 20:07:29 +0530

    Update XOROfNumberTwice.java
    XOR of Numbers appearing twice in an array

--- src/Leetcodes/XOROfNumberTwice.java
+++ src/Leetcodes/XOROfNumberTwice.java
@@ -13,7 +13,7 @@ public class XOROfNumberTwice {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if((nums[i]^nums[j])==0){
-                    count=count^nums[i];
+                    count=count^nums[i]; //XOR of result
                 }
             }
         }
