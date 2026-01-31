34f5b4fe3387aaa6a690d7e85050718d704d6011
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-01-31 23:35:01 +0530

    Sqrt of Number By Newton's Raphson Method

---
+++ src/Leetcodes/SqrtOfNumber.java
@@ -0,0 +1,24 @@
+package Leetcodes;
+
+//https://leetcode.com/problems/sqrtx/?envType=problem-list-v2&envId=math
+
+//By Newton's Raphson Method i.e √x = (n+(x/n))*0.5 where n is any guess we make 
+//loop that takes value of guess close to root where absolute error is less than 0.5 
+
+public class SqrtOfNumber {
+    public static void main(String[] args) {
+      System.out.println(mySqrt(8));  
+    }
+    public static int mySqrt(int x){
+        double n=x/2;  //n be any guess number
+        double root;
+        while(true){
+            root=0.5*(n+(x/n));
+            if(Math.abs(root-n)<0.5){
+                break;
+            }
+            n=root;
+        }
+        return (int)root;
+    }
+}
