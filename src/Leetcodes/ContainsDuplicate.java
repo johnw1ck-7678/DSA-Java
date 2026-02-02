ac084477538bdab16ca301275c0a4620da84c8e2
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-02-02 18:13:09 +0530

    Contains Duplicate

---
+++ src/Leetcodes/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package Leetcodes;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+       int[] arr = {1,2,3,1};
+       System.out.println(containsDuplicate(arr));
+    }
+    public static boolean containsDuplicate(int[] nums){
+        HashSet<Integer> set=new HashSet<>();
+        for(int x:nums){
+            if(!set.add(x)){
+                return true;
+            }
+        }
+        return false;
+        
+
+    }
+}
