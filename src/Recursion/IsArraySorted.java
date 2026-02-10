0ba091f6ba019b832690b0634d77cd2c9d99b70f
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-02-10 14:33:11 +0530

    Is A Given Array Sorted?

---
+++ src/Recursion/IsArraySorted.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class IsArraySorted {
+    public static void main(String[] args) {
+        int[] A={1,3,4,8,9,12};
+        System.out.println(sortArray(A, 1));
+    }
+    public static boolean sortArray(int[] arr,int index){
+        if(index==arr.length){
+            return true;
+        }
+        return ((arr[index-1]<arr[index])&&(sortArray(arr, index+1)));
+    }
+}
