8e740fbfed288b313122ec36c56713b893e0d165
johnw1ck-7678 <shreeyash.bhosale7@gmail.com>
2026-02-10 16:31:07 +0530

    Patterns Using Recursion

---
+++ src/Recursion/Patterns.java
@@ -0,0 +1,50 @@
+package Recursion;
+
+public class Patterns {
+    //1st -- 
+    /* *****
+       ****
+       ***
+       **
+       *
+     */
+    //2nd
+    /*
+        *
+        **
+        ***
+        ****
+        *****
+    */
+
+    public static void main(String[] args) {
+        first(5, 0);
+        second(5, 0);
+    }
+    static void first(int row,int column){
+        if(row==0){
+            return;
+        }
+        if(column<row){
+            System.out.print("*");
+            first(row, column+1);
+        }else{
+            System.out.println();
+            first(row-1, 0);
+        }
+    }
+    static void second(int row,int column){
+        if(row==0){
+            return;
+        }
+        if(column<row){
+            second(row, column+1);
+            System.out.print("*");
+        }else{
+            second(row-1,0);
+            System.out.println();
+        }
+    }
+
+
+}
