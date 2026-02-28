1bdf58020dbc7f4c97a7d5a96de8bc6bd9f27fe4
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-02-28 22:23:26 +0530

    Obtaining Subset using Recursion

---
+++ src/Recursion/Subset.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class Subset {
+    public static void main(String[] args) {
+       String string="abc";
+        subset("",string);
+    }
+    public static void subset(String p,String u){
+        if(u.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=u.charAt(0);
+        subset(p+ch, u.substring(1));
+        subset(p,u.substring(1));
+        
+    }
+}
