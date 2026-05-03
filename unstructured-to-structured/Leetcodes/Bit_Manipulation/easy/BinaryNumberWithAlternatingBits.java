905b79d7aba7aed5d3ec05a5169cb7ed821ed4be
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:44:06 +0530

    binary number with alternating bits

---
+++ unstructured-to-structured/Leetcodes/Bit_Manipulation/easy/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/binary-number-with-alternating-bits/description/?envType=daily-question&envId=2026-02-18
+//Number right shifted to 1 and XOR gives all 1's 
+//all 1's added to 1 and AND with 0 gives 0
+
+public class BinaryNumberWithAlternatingBits {
+    public static void main(String[] args) {
+        System.out.println(hasAlternatingBits(5));
+    }
+    public static boolean hasAlternatingBits(int n) {
+        int a=n^(n>>1);
+        if((a&(a+1))==0){
+            return true;
+        }
+        return false;
+    }
+}
