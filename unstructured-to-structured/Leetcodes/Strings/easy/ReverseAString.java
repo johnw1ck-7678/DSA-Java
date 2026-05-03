bd1cda63ec8b601ac5af0039e29e63d6ce1896f4
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:57:05 +0530

    Reversing a string

---
+++ unstructured-to-structured/Leetcodes/Strings/easy/ReverseAString.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+//https://leetcode.com/problems/reverse-string/description/
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+    }
+    public static void reverseString(char[] s){
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+        System.out.println(Arrays.toString(s));
+    }
+}
