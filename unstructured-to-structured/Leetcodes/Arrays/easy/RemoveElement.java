8411ffa5c81a2b4ad69cd4638a5bbbf4d65e16f0
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:33:53 +0530

    Remove Element from an Array

---
+++ unstructured-to-structured/Leetcodes/Arrays/easy/RemoveElement.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/remove-element/description/
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3, 4, 3};
+        int val = 3;
+
+        int k = removeElement(nums, val);
+
+        System.out.println("k = " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
