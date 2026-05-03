a74569a2773ed9e0beea26b678330249577497b5
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:39:33 +0530

    peak-index-in-a-mountain-array

---
+++ unstructured-to-structured/Leetcodes/Arrays/medium/PeakIndex.java
@@ -0,0 +1,24 @@
+
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+public class PeakIndex {
+    public static void main(String[] args) {
+        int[] A1={3,4,5,1};
+        System.out.println(peakIndexInMountainArray(A1));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }else if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+        }
+
+        return start;
+    }
+}
