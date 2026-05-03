ff427880d9358192dab159dddf296f0a83b062f9
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:34:49 +0530

    Square the Array and then sort it

---
+++ unstructured-to-structured/Leetcodes/Arrays/easy/SquareOfArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+public class SquareOfArray {
+    public static void main(String[] args) {
+        int[] A1={2,5,3,8,11};
+        System.out.println(Arrays.toString(square(A1)));
+
+    }
+    public static int[] square(int[] arr){
+        int[] A2=new int[arr.length];
+        for(int i=0;i<=arr.length-1;i++){
+            A2[i]=arr[i]*arr[i];
+        }
+        
+        Arrays.sort(A2);
+        return A2;
+    }
+}
