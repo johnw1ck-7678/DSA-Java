d5701c941e1608324edd95a1d20b1f84d703b7a5
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:29:50 +0530

    Flipping and Inverting the Image

---
+++ unstructured-to-structured/Leetcodes/Arrays/easy/FlippingAnImage.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+//https://leetcode.com/problems/flipping-an-image/description/
+
+public class FlippingAnImage {
+    public static void main(String[] args) {
+        int[][] A={{1,1,0},{1,0,1},{0,0,0}};
+        System.out.println(Arrays.deepToString(flipAndInvertImage(A)));
+    }
+    public static int[][] flipAndInvertImage(int[][] image){
+
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image[0].length;j++){
+                image[i][j]=image[i][j]^1;
+            }
+        }
+
+        for(int i=0;i<image.length;i++){
+            int a=0;
+            int b=image[0].length-1;
+            while(a<b){
+                int temp=image[i][a];
+                image[i][a]=image[i][b];
+                image[i][b]=temp;
+                a++;
+                b--;
+            }
+        }
+        return image;
+    }
+}
