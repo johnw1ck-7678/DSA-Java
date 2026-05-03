9205042947985ec9150093bc18e79b608e4b0011
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-03 14:44:28 +0530

    Complement Of a Base10 Integer

---
+++ unstructured-to-structured/Leetcodes/Bit_Manipulation/easy/ComplementOfBase10Integer.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/complement-of-base-10-integer/description/
+
+public class ComplementOfBase10Integer {
+    public static void main(String[] args) {
+        System.out.println(bitwiseComplement(10));
+    }
+    public static int bitwiseComplement(int n) {
+        if(n==0){
+            return 1;
+        }
+        int i=1;
+        while(i<=n){
+            i=i<<1;
+        }
+        return n^(i-1);
+    }
+}
