141658f8980ea5f207336c89826cfef30cbc59d3
Shreeyash Bhosale <shreeyash.bhosale7@gmail.com>
2026-05-16 10:25:09 +0530

    Search in Rotated Sorted Array

--- unstructured-to-structured/Leetcodes/Arrays/medium/RotatedSortedArray.java
+++ unstructured-to-structured/Leetcodes/Arrays/medium/RotatedSortedArray.java
@@ -1,7 +1,7 @@
 
 //https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
-public class RotateSortedArray {
+public class RotatedSortedArray {
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2} ;
         int target = 0;
